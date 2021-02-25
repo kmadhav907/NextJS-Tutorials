9c6e51343736136eb9dc5944459d425588f54e6c
kmadhav907 <kmadhav907@gmail.com>
2021-02-25 20:32:18 +0530

    Added Head

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,29 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Link from 'next/link'
+import Head from 'next/head'
 const NotFound = () => {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 5000)
+  }, [])
   return (
-    <div className='notFound'>
-      <h1>Oops!! Page Not Found</h1>
-      <p>
-        Go Back to HomePage <br />
-      </p>
-      <Link href='/'>
-        <a>Home</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>No Page Found</title>
+      </Head>
+      <div className='notFound'>
+        <h1>Oops!! Page Not Found</h1>
+        <p>
+          Go Back to HomePage <br />
+        </p>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+      </div>
+    </>
   )
 }
 
--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,27 @@
-import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 const About = () => {
   return (
-    <div>
-      <h1>Hello World </h1>
-      <p>
-        {' '}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam{' '}
-      </p>
-      <p>
-        {' '}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam{' '}
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <div>
+        <h1>Hello World </h1>
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam{' '}
+        </p>
+        <p>
+          {' '}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam{' '}
+        </p>
+      </div>
+    </>
   )
 }
 
