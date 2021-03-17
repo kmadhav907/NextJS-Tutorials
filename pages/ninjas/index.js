93cd20f4b0cac4ef6399d960baa32a205f1d62ef
kmadhav907 <kmadhav907@gmail.com>
2021-03-17 18:07:01 +0530

    added API

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,15 @@
 import Head from 'next/head'
-const Ninja = () => {
+import styles from '../../styles/Ninja.module.css'
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+  return {
+    props: {
+      ninjas: data,
+    },
+  }
+}
+const Ninja = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -7,6 +17,15 @@ const Ninja = () => {
       </Head>
       <div>
         <h1> All Ninjas </h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            {' '}
+            <a className={styles.single}>
+              {' '}
+              <h3>{ninja.name}</h3>
+            </a>{' '}
+          </div>
+        ))}
       </div>
     </>
   )
