102534ea326e57b151bfd549138761094272613b
kmadhav907 <kmadhav907@gmail.com>
2021-03-17 18:24:45 +0530

    added Each Link for User

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,34 +1,28 @@
-import Head from 'next/head'
 import styles from '../../styles/Ninja.module.css'
+import Link from 'next/link'
+
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
   const data = await res.json()
+
   return {
-    props: {
-      ninjas: data,
-    },
+    props: { ninjas: data },
   }
 }
-const Ninja = ({ ninjas }) => {
+
+const Ninjas = ({ ninjas }) => {
   return (
-    <>
-      <Head>
-        <title>Ninja List</title>
-      </Head>
-      <div>
-        <h1> All Ninjas </h1>
-        {ninjas.map((ninja) => (
-          <div key={ninja.id}>
-            {' '}
-            <a className={styles.single}>
-              {' '}
-              <h3>{ninja.name}</h3>
-            </a>{' '}
-          </div>
-        ))}
-      </div>
-    </>
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
   )
 }
 
-export default Ninja
+export default Ninjas
